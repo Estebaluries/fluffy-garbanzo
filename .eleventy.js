f3779d187597fa1a933fa9b30c87cfcdd9cbfa53
Aluries <189800501+Estebaluries@users.noreply.github.com>
2025-10-04 20:45:43 -0600

    mas cambios

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,15 @@
 module.exports = function(eleventyConfig) {
-
- 
-  // Copy static assets to _site
-  eleventyConfig.addPassthroughCopy({ "src/styles": "styles" });
-  eleventyConfig.addPassthroughCopy({ "src/scripts": "scripts" });
-  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
-
-
-
-
-
-
-
-
+  // Copy static folders to output
+  eleventyConfig.addPassthroughCopy("src/styles");
+  eleventyConfig.addPassthroughCopy("src/scripts");
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
     dir: {
-      input: "src",        // your source folder
-      includes: "includes", // tells Eleventy to look in /src/includes/
-      output: "_site"       // build output
+      input: "src",        // main source
+      includes: "includes",// templates live here
+      data: "data",        // global data folder
+      output: "_site"      // build output
     }
   };
 };
